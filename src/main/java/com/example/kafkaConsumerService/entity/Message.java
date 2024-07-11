12086705d4c2f21430ecd7e81e5c8d126616f247
aramtadevosyan <aramtad2006@gmail.com>
2024-07-11 17:34:43 +0400

    Implemented logging logic

--- src/main/java/com/example/kafkaConsumerService/entity/Message.java
+++ src/main/java/com/example/kafkaConsumerService/entity/Message.java
@@ -26,4 +26,12 @@ public class Message {
     public void setContent(String content) {
         this.content = content;
     }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/kafkaConsumerService/service/KafkaMessageListener.java
+++ src/main/java/com/example/kafkaConsumerService/service/KafkaMessageListener.java
@@ -1,12 +1,16 @@
 package com.example.kafkaConsumerService.service;
 
 import com.example.kafkaConsumerService.entity.Message;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaMessageListener {
+    private static final Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
     private final MessageService messageService;
 
     @Autowired
@@ -15,7 +19,14 @@ public class KafkaMessageListener {
     }
 
     @KafkaListener(topics = "messages")
-    public void listen(Message message) {
-        messageService.addMessage(message);
+    public void listen(ConsumerRecord<String, Message> record) {
+        log.info("Received new message: topic={}, partition={}, offset={}, key={}, value={}", record.topic(), record.partition(), record.offset(), record.key(), record.value());
+
+        try {
+            messageService.addMessage(record.value());
+            log.info("Successfully processed and saved the message with key={}", record.key());
+        } catch (Exception e) {
+            log.error("Error processing message with key={}, value={}", record.key(), record.value(), e);
+        }
     }
 }
